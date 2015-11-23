f1960845e7439cdb29beb37b6c70b13f9b28c7a5
magrimm <marius.grimm93@web.de>
2015-11-23 01:41:49 +0000

    object recognition

---
+++ object_recognition/src/header_object_recognition.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <ros/ros.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/Marker.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/common/common.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/spin_image.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+//#include <boost/bind.hpp>
+//#include <boost/bind/placeholders.hpp>
+
+//#include </home/marius/catkin_ws/src/mgrimm/object_recognition/src/pointcloud_subscriber.cpp>
+//#include </home/marius/catkin_ws/src/mgrimm/object_recognition/src/pointcloud_publisher.cpp>
