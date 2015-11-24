e991b28014d025f9d6d8f6bb6853729846c829cd
magrimm <marius.grimm93@web.de>
2015-11-24 07:11:14 +0000

    restructuring classes2

--- object_recognition/include/cloud_handling.h
+++ object_recognition/include/cloud_handling.h
@@ -11,9 +11,12 @@
 #include <parameter/parameter_bag.h>
 #include <cloud_filter.h>
 #include <cloud_segmentation.h>
+#include <cloud_matching.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <visualization_msgs/Marker.h>
+#include <pcl/common/common.h>
 
 class cloud_handling
 {
@@ -21,7 +24,10 @@ public:
 	cloud_handling (ros::NodeHandle nh, parameter_bag parameter);
 	void Match ();
 	void Callback (const sensor_msgs::PointCloud2ConstPtr& a_cloud_msg);
-	void LoadFromPCD ();
+	void features_of_objects ();
+	void visualize_marker (pcl::PointCloud<pcl::PointXYZ>::Ptr a_cloud_cluster,
+						   visualization_msgs::Marker::Ptr a_marker,
+						   int a_marker_id, float color_r, float color_g, float color_b);
 private:
 	parameter_bag parameter;
 	ros::NodeHandle nh;
