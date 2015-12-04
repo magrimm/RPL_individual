7af332c5001e727724f1a10a35eefd1214fb0c0b
magrimm <marius.grimm93@web.de>
2015-12-04 16:12:14 +0000

    add localization/

---
+++ localization/include/parameter/parameter_bag.h
@@ -0,0 +1,18 @@
+/*
+ * parameter_bag.h
+ *
+ *  Created on: Dec 4, 2015
+ *      Author: marius
+ */
+
+#ifndef _PARAMETER_BAG_H_
+#define _PARAMETER_BAG_H_
+
+#include <string>
+
+struct parameter_bag
+{
+	std::string test;
+};
+
+#endif /* _PARAMETER_BAG_H_ */
